9caacbf947abbb1267674b7fc4282cd78f073ac4
lmoffat <leonmfft@gmail.com>
2021-01-21 11:15:02 +0100

    Add JS files

--- footer.php
+++ footer.php
@@ -3,12 +3,12 @@
     <?php wp_footer(); ?>
 
     <script src="https://kit.fontawesome.com/5602f04557.js" crossorigin="anonymous"></script>
-    <script src="src/js/main-menu.js"></script>
-    <script src="src/js/customisation.js"></script>
+    <script src="dist/js/main-menu.js"></script>
+    <script src="dist/js/customisation.js"></script>
 
-    <script src="src/js/libraries/ScrollMagic.min.js"></script>
+    <script src="dist/js/libraries/ScrollMagic.min.js"></script>
     
-   <script src="src/js/scrollmagic.js"></script>
+   <script src="dist/js/scrollmagic.js"></script>
 </body>
 
 </html>
