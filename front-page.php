80b7f390d15440cbbf617c7ffd36741e26da84b8
Jennifer <jennifer-a@outlook.fr>
2021-01-21 14:21:40 +0100

    Tests de champs ACF pour bannières etc
    voilà

--- front-page.php
+++ front-page.php
@@ -1,37 +1,41 @@
 <?php 
     /** @link https://developer.wordpress.org/reference/functions/get_header/ */
     get_header();
+?>
+    <div class="row">
+    <?php if (have_rows('title_img_desc')) :
+        while (have_rows('title_img_desc')) : the_row(); ?>
+        <div class="col-6">
+            <h2 class="title_h2"><?php echo get_sub_field('title'); ?></h2>
+            <p><?php echo get_sub_field('desc'); ?></p>
+        </div>
+        <div class="col-6"><img src="<?php echo get_sub_field('img')['sizes']['medium']; ?>"></div>
+        <?php endwhile;
+    endif; ?>
+    </div>
 
-    /** @link https://developer.wordpress.org/themes/basics/the-loop/  */
-    if( have_posts() ): while( have_posts() ) : the_post();
-
-        $postID = get_the_ID();
-        $postData = get_post($postID);
-
-        $tagline = get_field("nomdechamps");
-
-
-        // Check rows exists.
-        if( have_rows('repeater_field_name') ):
-
-            // Loop through rows.
-            while( have_rows('repeater_field_name') ) : the_row();
-
-                // Load sub field value.
-                $sub_value = get_sub_field('sub_field');
-                
-                // Do something...
-
-            // End loop.
-            endwhile;
-
-        // No value.
-        else :
-            // Do something...
-        endif;
-    endwhile;
-    endif;
-
+    <div>
+    <?php if (have_rows('circles_functionalities')) :
+        while (have_rows('circles_functionalities')) : the_row(); ?>
+        <div>
+            <h2 class="title_h2"><?php echo get_sub_field('title'); ?></h2>
+        </div>
+        <div class="row">
+        <?php if (have_rows('circles_functionalities_functionalities')) :
+        while (have_rows('circles_functionalities_functionalities')) : the_row(); ?>
+            <div class="col-3">
+            <img src="<?php echo get_sub_field('img')['sizes']['medium']; ?>">
+            <h2 class="title_h2"><?php echo get_sub_field('title'); ?></h2>
+            <p><?php echo get_sub_field('desc'); ?></p>
+            </div>
+        <?php endwhile;
+    endif; ?>
+        </div>
+        <?php endwhile;
+    endif; ?>
+    </div>
+    
+<?php
     /** @link https://developer.wordpress.org/reference/functions/get_footer/ */
     get_footer();
 ?>
