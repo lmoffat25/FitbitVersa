6cc4b5ea31e82b91dc9b61c1dd20369180f3588e
Jennifer <jennifer-a@outlook.fr>
2021-01-22 13:54:21 +0100

    Création template part newsletter
    (et quelques petites autres modifs, sur top-banner)

--- footer.php
+++ footer.php
@@ -1,6 +1,26 @@
-    </div>
-    <?php /** @link https://developer.wordpress.org/reference/functions/wp_footer/ */ ?>
-    <?php wp_footer(); ?>
-</body>
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package versa-theme
+ */
+?>
 
+<footer class="footer col-10 centerHz">
+    <p class="footer__copyright">Copyright © 2020 Fitbit Inc. All rights reserved.</p>
+    <div class="footer__content">
+        <a href="" class="footer__item">CGV</a>
+        <a href="" class="footer__item">CGU</a>
+        <a href="" class="footer__item">Mentions légales</a>
+        <a href="" class="footer__item">Politique de confidentialité</a>
+    </div>
+</footer>
+
+<?php wp_footer(); ?>
+
+</body>
 </html>
