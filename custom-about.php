9a3b5c8b8fbc61b6ce989a73cd278db9e04adb2b
lmoffat <leonmfft@gmail.com>
2021-01-22 14:46:18 +0100

    Added about page

--- custom-about.php
+++ custom-about.php
@@ -1,2 +1,56 @@
 <?php
 /* Template Name: About */
+
+ wp_head();
+?>
+<main class="about-page">
+    <h1 class="about-page__title"><?php echo get_the_title() ?></h1>
+
+    <section class="about-section scroll-about">
+                <div class="trigger" id="trigger1"></div>
+                <div class="trigger" id="trigger2"></div>
+                <div class="trigger" id="trigger3"></div>
+                <div class="trigger" id="trigger4"></div>
+
+                <div id="pin1">
+                    <div class="about-section__container" id="txt1">
+                        <div class="about-section__content">
+                            <h2 class="about-section__title">2018</h2>
+                            <p class="about-section__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro ad, sint numquam molestias assumenda maiores?</p>
+                            <img class="about-section__img" src="http://localhost/theme-alex-arnaud/wp-content/uploads/stresse.png" alt="Une photo">    
+                        </div>
+                    </div>
+        
+        
+                    <div class="about-section__container" id="txt2">
+                        <div class="about-section__content">
+                            <h2 class="about-section__title">2019</h2>
+                            <p class="about-section__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro ad, sint numquam molestias assumenda maiores?</p>
+                            <img class="about-section__img" src="http://localhost/theme-alex-arnaud/wp-content/uploads/musique.png" alt="Une photo">    
+                        </div>
+                    </div>
+        
+                    <div class="about-section__container" id="txt3">
+                        <div class="about-section__content">
+                            <h2 class="about-section__title">2020</h2>
+                            <p class="about-section__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro ad, sint numquam molestias assumenda maiores?</p>
+                            <img class="about-section__img" src="http://localhost/theme-alex-arnaud/wp-content/uploads/dodo.png" alt="Une photo">    
+                        </div>
+                    </div>
+        
+                    <div class="about-section__container" id="txt4">
+                        <div class="about-section__content">
+                            <h2 class="about-section__title">2021</h2>
+                            <p class="about-section__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro ad, sint numquam molestias assumenda maiores?</p>
+                            <img class="about-section__img" src="http://localhost/theme-alex-arnaud/wp-content/uploads/sportive.png" alt="Une photo">    
+                        </div>
+                    </div>
+                </div>
+    </section>
+
+</main>
+
+
+<?php
+ wp_footer()
+ ?>
--- src/js/scrollmagic.js
+++ src/js/scrollmagic.js
@@ -1,32 +1,67 @@
+// SETUP ########################################################
 // init controller
-var controller = new ScrollMagic.Controller();
+console.log("load-scrollmagic");
+const controller = new ScrollMagic.Controller();
+let triggers = document.querySelectorAll(".trigger");
+var triggerlength = triggers.length;
+// Différentes Pages
+const avantage = document.querySelector(".scroll-avantage");
+const about = document.querySelector(".scroll-about");
+// Page actuelle
+var page = "";
+
+// Hauteur de l'écran
+var screenH = window.innerHeight;
+let offset = screenH / 6; // Décallage entre deux triggers
+
+
 
 var main = function() {
-    scroll();
-    active();
+    if ( triggers == undefined ) {
+        console.log("no triggers");
+        return;
+    }
+    // Vérifie la page actuelle
+    checkPage();
+    // Donne taille du scroll
+    if ( page === avantage ) {
+        var scrollLength = 1000;
+    }else if ( page === about ) {
+        var scrollLength = screenH * triggerlength;
+    }
+    // Applique la taille du scroll
+    scroll(scrollLength);
+    // Applique les effets aux éléments
+    active(scrollLength);
 };
 
-
-
-var scroll = function() {
-    var image = new ScrollMagic.Scene({triggerElement: "#trigger1", duration : 1000})
-                    .setPin("#pin1")
-                    //.addIndicators({name: "1 (duration: 1000)"}) // add indicators (requires plugin)
-                    .addTo(controller);
-
- /*   var txt1 = new ScrollMagic.Scene({triggerElement : "#trigTxt1", duration : 300})
-                    .setPin("#pintxt1")
-                    .addIndicators({name : "2 (duration : 300)"})
-                    .addTo(controller); */
+// FUNCTIONS #####################################
+var checkPage = function() {
+    if ( avantage != undefined ) {
+        page = avantage;
+    }else if ( about != undefined ) {
+        page = about;
+    }
+    return page;
 }
-var triggerlength = 4;
 
-var active = function() {
+
+var scroll = function(scrollLength) {
+    var image = new ScrollMagic.Scene({triggerElement: "#trigger1", duration : scrollLength})
+                    .setPin("#pin1")
+                    // .addIndicators({name: "1 (duration: "+ scrollLength +")"}) // add indicators (requires plugin)
+                    .addTo(controller);
+}
+
+
+var active = function(scrollLength) {
+    let scrollInterval = scrollLength / triggerlength;
     for( var i = 1; i < triggerlength + 1; i++ ) {
-        var image = new ScrollMagic.Scene({triggerElement: "#trigger" + i, duration : 400})
+        var image = new ScrollMagic.Scene({triggerElement: "#trigger" + i, duration : scrollInterval - offset})
         .setClassToggle("#txt" + i, "-active")
-        //.addIndicators({name: i + "(duration: 300)"})
+        // .addIndicators({name: i + "(duration:"+ scrollInterval +")"})
         .addTo(controller);
     }
 }
+
 main();
