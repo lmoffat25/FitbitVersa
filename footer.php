ea3556ee000734fb1e63ed807b01748bfaae3525
Jennifer <jennifer-a@outlook.fr>
2021-02-27 14:09:21 +0100

    Création des menus header et footer
    Et quelques autres petites modifs

--- footer.php
+++ footer.php
@@ -12,12 +12,20 @@
 
 <footer class="footer col-10 centerHz">
     <p class="footer__copyright">Copyright © 2020 Fitbit Inc. All rights reserved.</p>
-    <div class="footer__content">
+    <!--<div class="footer__content">
         <a href="" class="footer__item">CGV</a>
         <a href="" class="footer__item">CGU</a>
         <a href="" class="footer__item">Mentions légales</a>
         <a href="" class="footer__item">Politique de confidentialité</a>
-    </div>
+    </div>-->
+    <?php
+        if ( has_nav_menu( 'footer-menu' ) ) :
+        wp_nav_menu ( array (
+        'theme_location' => 'footer-menu' ,
+        'menu_class' => 'footer__content', // classe CSS pour customiser mon menu
+        ) );
+    endif;
+    ?>
 </footer>
 
 <?php wp_footer(); ?>
